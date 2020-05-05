2552f3817bb35bbf9aeeb48f4c30dbd2f3ba0c1b
hinzundcode <chris-git@labasch.net>
2020-05-05 19:10:01 +0200

    fix wrong toggle output

--- toggle.js
+++ toggle.js
@@ -2,19 +2,21 @@ module.exports = RED => {
 	function ToggleNode(config) {
 		RED.nodes.createNode(this, config);
 		
-		let setState = state => {
+		let updateState = state => {
 			this.context().set("state", state);
 			
 			if (state) this.status({ fill: "green", shape: "dot", text: "true" });
 			else this.status({ fill: "red", shape: "dot", text: "false" });
 		};
 		
-		setState(config.initialState === "true");
+		updateState(config.initialState === "true");
 		
 		this.on("input", msg => {
 			let state = this.context().get("state");
 			
-			setState(!state);
+			state = !state;
+			
+			updateState(!state);
 			
 			msg.payload = state;
 			this.send(msg);
