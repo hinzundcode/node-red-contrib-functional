ae7a3f018a29a9b58fb7807954f484785d7ec7ad
hinzundcode <chris-git@labasch.net>
2020-05-24 22:30:25 +0200

    add variable node

---
+++ variable.js
@@ -0,0 +1,21 @@
+module.exports = RED => {
+	function VariableNode(config) {
+		RED.nodes.createNode(this, config);
+		this.emitOnChange = config.emitOnChange;
+		
+		this.on("input", msg => {
+			if (msg._cmd == "get") {
+				msg.payload = this.context().get("value");
+				this.send(msg);
+			} else {
+				let valueBefore = this.context().get("value");
+				this.context().set("value", msg.payload);
+				
+				if (this.emitOnChange && (typeof valueBefore === "undefined" || JSON.stringify(msg.payload) != JSON.stringify(valueBefore)))
+					this.send(msg);
+			}
+		});
+	}
+	
+	RED.nodes.registerType("variable", VariableNode);
+};
