7cc564f68ff2bb9a46f62919d5fd1515bf08e166
hinzundcode <chris-git@labasch.net>
2020-05-24 22:48:04 +0200

    test

--- variable.js
+++ variable.js
@@ -1,19 +1,23 @@
 module.exports = RED => {
 	function VariableNode(config) {
 		RED.nodes.createNode(this, config);
-		this.emitOnChange = config.emitOnChange == "true";
+		this.emitOnChange = config.emitOnChange === "true";
 		
-		this.on("input", msg => {
+		this.on("input", (msg, send, done) => {
 			if (msg._cmd == "get") {
 				msg.payload = this.context().get("value");
-				this.send(msg);
+				send(msg);
 			} else {
 				let valueBefore = this.context().get("value");
 				this.context().set("value", msg.payload);
 				
 				if (this.emitOnChange && (typeof valueBefore === "undefined" || JSON.stringify(msg.payload) != JSON.stringify(valueBefore)))
-					this.send(msg);
+					send(msg);
+				else
+					send(null);
 			}
+			
+			if (done) done();
 		});
 	}
 	
